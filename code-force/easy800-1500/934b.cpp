c9b4caa70145967125f7cd0d75c0c86e93d6abf9
phamlong15297 <phamlong15297@gmail.com>
2021-05-29 11:07:42 +0700

    Update 934b.cpp

--- code-force/easy800-1500/934b.cpp
+++ code-force/easy800-1500/934b.cpp
@@ -1,28 +1,30 @@
 #include<bits/stdc++.h>
-
+ 
 using namespace std;
-
+ 
 typedef long long ll;
-
+ 
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
     ll n;
     cin >> n;
     int eight, four;
     eight = n /2;
     four = n % 2;
     string result = "";
-    for(int i=0;i<eight;i++){
-        result += "8";
-    }
-    for (int i=0;i<four;i++){
-        result +="4";
-    }
-    if (stof(result) > pow(10,18)){
+    if (n>36){
         cout << -1;
     } else {
+        for(int i=0;i<eight;i++){
+        result += "8";
+        }
+        for (int i=0;i<four;i++){
+            result +="4";
+        }
         cout << result;
     }
-    return 1;
+    return 0;
 }
 // #include <bits/stdc++.h>
  
