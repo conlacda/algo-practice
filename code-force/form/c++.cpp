b51f14e3f55e381f1e90f66efc1702828a88c6e4
phamlong15297 <phamlong15297@gmail.com>
2021-06-05 22:39:15 +0700

    thêm 180C

---
+++ code-force/form/c++.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll INF=(1LL<<60)-1;
+vector<ll> a,b;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    return 0;
+}
