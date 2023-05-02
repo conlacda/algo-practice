43d015b5b134ac98b4ffa37b4f4f0397fd49bd79
conlacda <phamlong15297@gmail.com>
2023-05-02 16:31:32 +0100

    Update 25E-Test.cpp

--- codeforces/hard-from-2200/25E-Test.cpp
+++ codeforces/hard-from-2200/25E-Test.cpp
@@ -1,5 +1,5 @@
-// https://codeforces.com/contest/25/submission/195569890
 // https://codeforces.com/contest/25/problem/E
+// https://codeforces.com/contest/25/my
 #include <bits/stdc++.h>
 
 typedef long long ll;
@@ -14,7 +14,7 @@ using namespace std;
 #define dbg(...)
 #endif
 
-<hash-string.snippet>
+<hash-string without changing any things>
 
 int main() {
     ios::sync_with_stdio(0);
@@ -26,7 +26,6 @@ int main() {
     string s1, s2, s3;
     Hash<string> hash;
     hash.build();
-    // while (cin >> s1){
     cin >> s1 >> s2 >> s3;
     auto two_string = [&](string a, string b) -> string {
         Hash<string> ha = hash;
@@ -54,30 +53,5 @@ int main() {
     ans = min(ans, cal(s3, s2, s1));
     ans = min(ans, cal(s3, s1, s2));
     cout << ans;
-    // }
     cerr << "Time : " << (double)clock() / (double)CLOCKS_PER_SEC << "s\n";
 }
-/*
-Sometimes it is hard to prepare tests for programming problems. Now Bob is preparing tests to new problem about strings — input data to his problem is one string. Bob has 3 wrong solutions to this problem. The first gives the wrong answer if the input data contains the substring s1, the second enters an infinite loop if the input data contains the substring s2, and the third requires too much memory if the input data contains the substring s3. Bob wants these solutions to fail single test. What is the minimal length of test, which couldn't be passed by all three Bob's solutions?
-
-Input
-There are exactly 3 lines in the input data. The i-th line contains string si. All the strings are non-empty, consists of lowercase Latin letters, the length of each string doesn't exceed 105.
-
-Output
-Output one number — what is minimal length of the string, containing s1, s2 and s3 as substrings.
-
-Examples
-input
-ab
-bc
-cd
-output
-4
-
-input
-abacaba
-abaaba
-x
-output
-11
-*/
