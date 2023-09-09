29a620b3b243d598795c8f1161409ec14f375f70
conlacda <phamlong15297@gmail.com>
2023-09-09 10:42:59 +0100

    Create PRIME1.cpp

---
+++ spoj/primes/PRIME1.cpp
@@ -0,0 +1,97 @@
+// https://www.spoj.com/problems/PRIME1/
+#include<bits/stdc++.h>
+
+typedef long long ll;
+using uint = unsigned int;
+using ull = unsigned long long;
+const ll mod = 1000000007; // 998244353  1000000009  1000000007 // đừng dùng ull
+#define ld long double
+#define int long long
+const int INF = std::numeric_limits<int>::max();
+
+using namespace std;
+
+#ifdef DEBUG
+#include "debug.cpp"
+#else
+#define dbg(...)
+#define show_exec_time()
+#define destructure(a) #a
+#endif
+
+int divCeil(int a, int b) {
+  return a / b + (((a ^ b) > 0 && a % b != 0) ? 1 : 0);
+}
+
+const int maxN = 1e7; // maxN < 1e8
+class Prime {
+    // hàm sàng nguyên tố, có thể tách ra độc lập nếu phần này rườm ra quá
+    void build(int n){
+        is_prime.set(); // set to full 1
+        is_prime[0] = is_prime[1] = false;
+        for (int i = 2; i * i <= n; i++)
+            if (is_prime[i])
+                for (int j = i * i; j <= n; j += i) is_prime[j] = false;
+        for (int i=0;i<n;i++) if (is_prime[i]) primes.push_back(i);
+    }
+public:
+    int n;
+    bitset<maxN> is_prime;
+    vector<int> primes;
+    Prime(int n = maxN) {
+        this->n = n;
+        build(n);
+    }
+    bool isPrime(int k) {
+        assert(k < n && "Out of range maxN");
+        return is_prime[k];
+    }
+    vector<int> primesList(int l, int r) {
+        int sz = r-l+1;
+        vector<bool> ip(sz+1, true);
+        for (int i=2;i*i<=r;i++) {
+            if (!is_prime[i]) continue;
+            for (int j = max(i * i, divCeil(l, i) * i); j <= r; j += i) ip[j-l] = false;
+        }
+        vector<int> res;
+        for (int i=0;i<sz;i++) {
+            if (ip[i]) {
+                if (i + l != 1) res.push_back(i+l);
+            } 
+        }
+        return res;
+    }
+};
+Prime p;
+
+void solve() {
+    int l, r;
+    cin >> l >> r;
+    for (auto v: p.primesList(l, r)) {
+        cout << v << '\n';
+    }
+    cout << '\n';
+}
+
+signed main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    #ifdef DEBUG
+        freopen("inp.txt", "r", stdin);
+        freopen("out.txt", "w", stdout);
+    #endif
+    cout << std::fixed << setprecision(15);
+    int n;
+    cin >> n;
+    while (n--) solve();
+
+    
+    show_exec_time();
+}
+/*
+Đọc chậm rãi
+Viết ra ý tưởng
+The pattern from simple input to output.
+
+TLE:
+    map vs unordered_map ??
+*/
