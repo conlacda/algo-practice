ecadd68a3d414cf32bf115cd026b2a11f3e01a82
conlacda <phamlong15297@gmail.com>
2023-11-04 01:03:53 +0900

    update Perfect number

--- codeforces/Codeforces Round 460 (Div. 2)/B. Perfect Number.cpp
+++ codeforces/Codeforces Round 460 (Div. 2)/B. Perfect Number.cpp
@@ -20,7 +20,8 @@ using namespace std;
 #define destructure(a) #a
 #endif
 
-// digitDP[index][digit][constraint][cur_sum]
+// highlight-next-line
+// digitDP[index][digit][constraint][cur_sum] - dùng digitDP 3 dimensions rồi thêm cur_sum vào
 int digitDP(int n) { // n = 3245
     /*
     Tính trước hàng đầu tiên
@@ -35,8 +36,10 @@ int digitDP(int n) { // n = 3245
     // Initial state
     string nstr = to_string(n);
     vector<vector<vector<vector<int>>>> dp(nstr.size(), vector<vector<vector<int>>>(10, vector<vector<int>>(2, vector<int>(12, 0))));
+    // highlight-start
     for (int i=0;i<nstr[0] - '0';i++) dp[0][i][0][i] = 1; // xxxx -> [0->2]xxx
     dp[0][nstr[0] - '0'][1][nstr[0] - '0'] = 1; // 3xxx
+    // highlight-end
     // Tính toán các chữ số đằng sau
     for (int index=1;index<(int) nstr.size();index++) {
         int prevdigit = nstr[index-1] - '0'; // digit tại số n
@@ -44,27 +47,30 @@ int digitDP(int n) { // n = 3245
         // Mọi số ko constraint, prev, cur không có, số được chọn có thể tùy ý từ 0..9 
         for (int p=0;p<=9;p++) {
             for (int c=0;c<=9;c++) {
-                for (int sum=0;sum<12;sum++) {
+                // highlight-start
+                for (int sum=0;sum<12;sum++)
                     dp[index][c][0][min(sum+c, (int)11)] += dp[index-1][p][0][sum]; // 2xxx -> 2[0:9]xx
-                }
+                // highlight-end
             }
         }
         // Constraint số đằng trước, số đằng sau ko constraint
         // 3245. prevdigit = 3, curdigit = 2 -> số đang có dạng 3xxx. c <=2. Trường hợp này xét c < 2. Khi này sẽ ko bị constraint nữa. 31xx -> xx tự do từ 00..99
         for (int c=0;c<curdigit;c++) {
-            for (int sum=0;sum<12;sum++) {
+            // highlight-start
+            for (int sum=0;sum<12;sum++)
                 dp[index][c][0][min(sum+c, (int) 11)] += dp[index-1][prevdigit][1][sum]; // 3xxx -> 3[0:1]xxx
-            }
+            // highlight-end
         }
         // Constraint số đằng trước và cả số đằng sau. 3245 -> prevdigit = 3, curdigit = 2.
-        for (int sum=0;sum<12;sum++) {
+        // highlight-start
+        for (int sum=0;sum<12;sum++)
             dp[index][curdigit][1][min(sum+curdigit, (int)11)] += dp[index-1][prevdigit][1][sum]; // 3xxx -> 32xx
-        }
+        // highlight-end
     }
     // In ra kết quả
-    int ans = 0; // sum(dp[n-1][all_digit][0/1])
+    int ans = 0; // sum(dp[n-1][all_digit][0/1][cur_sum = 10])
     for (int d=0;d<=9;d++) {
-        ans += dp[nstr.size()-1][d][0][10];
+        ans += dp[nstr.size()-1][d][0][10]; // thêm cur_sum = 10
         ans += dp[nstr.size()-1][d][1][10];
     }
     return ans;
