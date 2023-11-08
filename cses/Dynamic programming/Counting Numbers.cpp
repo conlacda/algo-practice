2bad6fc428b761129eeec051b92eec38dce8a8e9
conlacda <phamlong15297@gmail.com>
2023-11-08 23:02:34 +0900

    add highlight

--- cses/Dynamic programming/Counting Numbers.cpp
+++ cses/Dynamic programming/Counting Numbers.cpp
@@ -28,18 +28,23 @@ int digitDPSameDigit(string low, string up) {
     // Initial state
     // dp[index][digit][constraint_0/1/2/3]
     vector<vector<vector<int>>> dp(n, vector<vector<int>>(10, vector<int>(4, 0)));
+    // highlight-next-line
     vector<vector<vector<int>>> cnt(n, vector<vector<int>>(10, vector<int>(4, 0)));
     if (up[0] == low[0]) {
         dp[0][up[0] - '0'][3] = 1; // 1234 & 1567 => 1xxx
+        // highlight-next-line
         cnt[0][up[0] - '0'][3] = 1; // 1234 & 1567 => 1xxx
     } else {
         dp[0][low[0] - '0'][1] = 1; // 1234 & 3456 => 1xxx
+        // highlight-next-line
         cnt[0][low[0] - '0'][1] = 1; // 1234 & 3456 => 1xxx
         for (int first_digit=low[0]-'0'+1;first_digit<=up[0]-'0'-1;first_digit++) {
             dp[0][first_digit][0] = 1; // 2xxx
+            // highlight-next-line
             cnt[0][first_digit][0] = 1; // 2xxx
         }
         dp[0][up[0] - '0'][2] = 1; // 3xxx
+        // highlight-next-line
         cnt[0][up[0] - '0'][2] = 1; // 3xxx
     }
     /*
@@ -60,30 +65,35 @@ int digitDPSameDigit(string low, string up) {
             for (int p=0;p<10;p++) {
                 dp[index][digit][0] += dp[index-1][p][0];
                 if (p != digit)
+                    // highlight-next-line
                     cnt[index][digit][0] += cnt[index-1][p][0];
             }
             // Constraint trên => ko constraint
             if (digit < up[index] - '0') {
                 dp[index][digit][0] += dp[index-1][up[index-1] - '0'][2];
                 if (digit != up[index-1] - '0')
+                    // highlight-next-line
                     cnt[index][digit][0] += cnt[index-1][up[index-1] - '0'][2];
             }
             // Constraint trên => tiếp tục constraint
             if (digit == up[index] - '0') {
                 dp[index][digit][2] += dp[index-1][up[index-1] - '0'][2];
                 if (digit != up[index-1] - '0')
+                // highlight-next-line
                     cnt[index][digit][2] += cnt[index-1][up[index-1] - '0'][2];
             }
             // Constraint dưới => ko constraint
             if (digit > low[index] - '0') {
                 dp[index][digit][0] += dp[index-1][low[index-1] - '0'][1];
                 if (digit != low[index-1] -'0')
+                // highlight-next-line
                    cnt[index][digit][0] += cnt[index-1][low[index-1] - '0'][1];
             }
             // Constraint dưới => constraint dưới tiếp
             if (digit == low[index] - '0') {
                 dp[index][digit][1] += dp[index-1][low[index-1] - '0'][1];
                 if (digit != low[index-1] - '0')
+                // highlight-next-line
                     cnt[index][digit][1] += cnt[index-1][low[index-1] - '0'][1];
             }
             // Constraint 2 chiều => ko constraint
@@ -95,6 +105,7 @@ int digitDPSameDigit(string low, string up) {
                 if (digit == up[index] - '0') {
                     dp[index][digit][3] += dp[index-1][prevdigit][3];
                     if (digit != prevdigit)
+                    // highlight-next-line
                         cnt[index][digit][3] += cnt[index-1][prevdigit][3];
                 }
             } else {
@@ -102,18 +113,21 @@ int digitDPSameDigit(string low, string up) {
                 if (digit == low[index] - '0') {
                     dp[index][digit][1] += dp[index-1][prevdigit][3];
                     if (digit != prevdigit)
+                    // highlight-next-line
                         cnt[index][digit][1] += cnt[index-1][prevdigit][3];
                 }
                 // không constraint nữa
                 if (low[index] - '0' < digit && digit < up[index] - '0') {
                     dp[index][digit][0] += dp[index-1][prevdigit][3];
                     if (digit != prevdigit)
+                    // highlight-next-line
                         cnt[index][digit][0] += cnt[index-1][prevdigit][3];
                 }
                 // constraint trên
                 if (digit == up[index] - '0') {
                     dp[index][digit][2] += dp[index-1][prevdigit][3];
                     if (digit != prevdigit)
+                    // highlight-next-line
                         cnt[index][digit][2] += cnt[index-1][prevdigit][3];
                 }
             }
