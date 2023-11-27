f3a4c95dd6d942f5ee4fd4db9a2ead66d71f6123
conlacda <phamlong15297@gmail.com>
2023-11-27 22:39:30 +0900

    Update G - Triple Index.cpp

--- atcoder/BeginnerContest/293/G - Triple Index.cpp
+++ atcoder/BeginnerContest/293/G - Triple Index.cpp
@@ -1,10 +1,9 @@
 // https://atcoder.jp/contests/abc293/tasks/abc293_g
-// https://atcoder.jp/contests/abc293/submissions/39611628 - reference
 #include<bits/stdc++.h>
 
 typedef long long ll;
 const ll mod = 1e9 + 7;
-#define ld long double
+#define int long long // __int128
 
 using namespace std;
 
@@ -15,35 +14,33 @@ using namespace std;
 #define destructure(a) #a
 #endif
 
-inline ll gilbertOrder(int x, int y, int pow, int rotate) {
-    if (pow == 0) {
-        return 0;
-    }
+
+inline int64_t hilbertOrder(int x, int y, int pow = 21, int rotate = 0) { // 2**pow là số lượng query sẽ xử lý
+    if (pow == 0) return 0;
     int hpow = 1 << (pow-1);
     int seg = (x < hpow) ? ((y < hpow) ? 0 : 3) : ((y < hpow) ? 1 : 2);
     seg = (seg + rotate) & 3;
     const int rotateDelta[4] = {3, 0, 0, 1};
     int nx = x & (x ^ hpow), ny = y & (y ^ hpow);
     int nrot = (rotate + rotateDelta[seg]) & 3;
-    ll subSquareSize = int64_t(1) << (2*pow - 2);
-    ll ans = seg * subSquareSize;
-    ll add = gilbertOrder(nx, ny, pow-1, nrot);
+    int64_t subSquareSize = int64_t(1) << (2*pow - 2);
+    int64_t ans = seg * subSquareSize;
+    int64_t add = hilbertOrder(nx, ny, pow-1, nrot);
     ans += (seg == 1 || seg == 2) ? add : (subSquareSize - add - 1);
     return ans;
 }
+
 struct Query {
-    ll l, r, index;
-    int64_t ord = -1;
+    int l, r, index;
+    ll ord = -1;
     friend bool operator<(Query& a, Query& b) {
-        if (a.ord == -1) a.ord = gilbertOrder(a.l, a.r, 21, 0);
-        if (b.ord == -1) b.ord = gilbertOrder(b.l, b.r, 21, 0);
         return a.ord < b.ord;
     }
     friend std::ostream& operator<<(std::ostream& os, const Query &s) { return os << destructure(s);}
 };
 struct Ans {
     ll index, val;
-    friend bool operator<(Ans a, Ans b) {
+    friend bool operator<(Ans& a, Ans& b) {
         return a.index < b.index;
     }
     friend std::ostream& operator<<(std::ostream& os, const Ans &s) { return os << destructure(s);}
@@ -51,12 +48,12 @@ struct Ans {
 
 struct Mo {
 private:
-    vector<ll> a; // mảng đầu vào
+    vector<int> a; // mảng đầu vào
 public:
-    vector<ll> data; // lưu trạng thái hiện tại - lưu tần số của từng số có trong range [left, right] hiện tại
+    vector<int> data; // lưu trạng thái hiện tại - lưu tần số của từng số có trong range [left, right] hiện tại
     vector<Ans> ans; // index, val
-    ll cur_result = 0;
-    Mo(vector<ll> a) {
+    int cur_result = 0; // giá trị hiện tại, đối với những bài giá trị tuyến tính khi add, remove thì dùng, ko thì tính trực tiếp từ mảng data
+    Mo(vector<int>& a) {
         this->a = a;
         data.resize(200005, 0);
     }
@@ -73,10 +70,9 @@ public:
     ll getResult() {
         return this->cur_result;
     }
-    void solve(vector<Query> queries) {
-        // for (auto& v: queries) v.calcOrder();
+    void solve(vector<Query>& queries) {
+        for (auto& q: queries) q.ord = hilbertOrder(q.l, q.r);
         sort(queries.begin(), queries.end());
-        dbg(queries);
         ll left = 0, right = -1;
         for (auto query: queries) {
             while (left > query.l) left--, add(left);
@@ -89,8 +85,7 @@ public:
     }
 };
 
-
-int main(){
+signed main(){
     ios::sync_with_stdio(0); cin.tie(0);
     #ifdef DEBUG
         freopen("inp.txt", "r", stdin);
