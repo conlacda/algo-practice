9945305db74492a1da962594101927904e89f601
conlacda <phamlong15297@gmail.com>
2023-11-28 20:58:55 +0900

    Update COT2 - Count on a tree II.cpp

--- spoj/mo's-algorithm/COT2 - Count on a tree II.cpp
+++ spoj/mo's-algorithm/COT2 - Count on a tree II.cpp
@@ -3,7 +3,7 @@
 
 typedef long long ll;
 const ll mod = 1e9 + 7;
-#define ld long double
+#define int long long // __int128
 
 using namespace std;
 
@@ -43,17 +43,30 @@ struct Compress {
     }
 };
 
-const ll blockSize = 300; // 300, 700
+inline int64_t hilbertOrder(int x, int y, int pow = 21, int rotate = 0) { // 2**pow là số lượng query sẽ xử lý
+    if (pow == 0) return 0;
+    int hpow = 1 << (pow-1);
+    int seg = (x < hpow) ? ((y < hpow) ? 0 : 3) : ((y < hpow) ? 1 : 2);
+    seg = (seg + rotate) & 3;
+    const int rotateDelta[4] = {3, 0, 0, 1};
+    int nx = x & (x ^ hpow), ny = y & (y ^ hpow);
+    int nrot = (rotate + rotateDelta[seg]) & 3;
+    int64_t subSquareSize = int64_t(1) << (2*pow - 2);
+    int64_t ans = seg * subSquareSize;
+    int64_t add = hilbertOrder(nx, ny, pow-1, nrot);
+    ans += (seg == 1 || seg == 2) ? add : (subSquareSize - add - 1);
+    return ans;
+}
 
 struct Query {
-    ll l, r, index;
-    friend bool operator<(Query a, Query b) {
-        // những cái thuộc cùng 1 block sẽ đứng cạnh nhau, những cái cùng block thì r sẽ xếp từ bé tới lớn
-        if (a.l/blockSize == b.l/blockSize) return a.r < b.r;
-        return a.l/blockSize < b.l/blockSize;
+    int l, r, index;
+    ll ord = -1;
+    friend bool operator<(Query& a, Query& b) {
+        return a.ord < b.ord;
     }
     friend std::ostream& operator<<(std::ostream& os, const Query &s) { return os << destructure(s);}
 };
+
 struct Ans {
     ll index, val;
     friend bool operator<(Ans a, Ans b) {
@@ -66,8 +79,7 @@ struct MoOnTree {
 private:
     template<typename LL>class LCA{struct Euler{LL vertex, height, index;};template<typename T>class LCASegmentTree{private:ll n;vector<T>dat;public:T merge(T a,T b){if(a.height>b.height)return b;return a;}LCASegmentTree(vector<T>v){int _n=v.size();n=1;while(n<_n)n*=2;dat.resize(2*n-1);for(int i=0;i<_n;i++)dat[n+i-1]=v[i];for(int i=n-2;i>=0;i--)dat[i]=merge(dat[i*2+1],dat[i*2+2]);} LCASegmentTree(int _n){n=1;while(n<_n)n*=2;dat.resize(2*n-1); } void set_val(int i,T x){i+=n-1;dat[i]=x;while(i>0){i=(i-1)/2;dat[i]=merge(dat[i*2+1],dat[i*2+2]);}}T query(int l,int r){r++;T left=T{INT_MAX,INT_MAX,INT_MAX},right=T{INT_MAX,INT_MAX,INT_MAX};l+=n-1;r+=n-1;while(l<r){if((l&1)==0)left=merge(left,dat[l]);if((r&1)==0)right=merge(dat[r-1],right);l=l/2;r=(r-1)/2;}return merge(left,right);}};public:int n;vector<vector<LL>> graph;vector<bool> visited;vector<Euler> eulertour, first;LCASegmentTree<Euler> *seg;LCA() {}LCA(vector<vector<LL>> graph, LL root = 0){build(graph, root);}void build(vector<vector<LL>> graph, LL root = 0) {this->graph = graph;n = graph.size();visited.resize(n);first.resize(n);makeEuler(root);}void makeEuler(LL root){std::fill(visited.begin(), visited.end(), false);int height =0;std::function<void(int)> explore = [&](int u){visited[u] = true;height++;eulertour.push_back(Euler{u, height, (int) eulertour.size()});for (auto v: this->graph[u]){if (!visited[v]) {explore(v);height--;eulertour.push_back(Euler{u, height, (int) eulertour.size()});}}};explore(root);std::fill(visited.begin(), visited.end(), false);for (auto e: eulertour){if (!visited[e.vertex]){visited[e.vertex] = true;first[e.vertex] = e;}}this->seg = new LCASegmentTree<Euler>(eulertour);}LL lca(LL u, LL v){LL uidx = first[u].index;LL vidx = first[v].index;if (uidx > vidx) swap(uidx, vidx);Euler a = seg->query(uidx, vidx);return a.vertex;}vector<LL> height(){vector<LL> h(this->n, 0);for (auto e: eulertour) h[e.vertex] = e.height;return h;}LL lca(LL r, LL u, LL v){LL ru = lca(r, u);LL rv = lca(r, v);LL uv = lca(u, v);if (ru == rv) return uv;if (ru == uv) return rv;return ru;}};
     std::vector<ll> first, second, eulertour;
-    vector<ll> makeEulerTour(vector<vector<ll>> adj) {
-        vector<ll> eulertour;
+    void makeEulerTour(vector<vector<ll>> adj) {
         first.resize(n); second.resize(n); // first[v] - thời điểm duyệt v, second[v] - thời điểm ra khỏi v
         ll clock = 0;
         std::function<void(ll, ll)> dfs = [&](ll u, ll p){
@@ -80,23 +92,21 @@ private:
             eulertour.push_back(u);        
         };
         dfs(0, 0);
-        return eulertour;
     }
-    // Chuyển query trên node sang query trên euler tour
-    vector<Query> fromEdgeToEuler(vector<Query> queries) {
-        vector<Query> res;
-        for (auto q: queries) {
-            ll u = q.l, v= q.r, idx = q.index;
+    void transformFromNodeToEuler(vector<Query>& queries) {
+        for (auto& q: queries) {
+            ll u = q.l, v= q.r;
             if (first[u] > first[v]) swap(u, v); 
             if (lca.lca(u, v) == u) {
                 // first(u) -> first(v) (má trái)
-                res.push_back(Query{first[u], first[v], idx});
+                q.l = first[u];
+                q.r = first[v];
             } else {
                 // second(u) -> first(v)
-                res.push_back({second[u], first[v], idx});
+                q.l = second[u];
+                q.r = first[v];
             }
         }
-        return res;
     }
 public:
     ll n;
@@ -112,29 +122,30 @@ public:
         this->weight = weight;
         data.resize(this->n, 0);
         lca.build(adj);
-        eulertour = makeEulerTour(adj);
+        makeEulerTour(adj);
         vis.resize(n, false);
     }
 
     void add(ll index) {
-        ll edge = eulertour[index];
-        if (data.at(weight.at(edge)) == 0) cur_result++;
-        data[weight[edge]]++;
+        ll node = eulertour[index];
+        if (data.at(weight[node]) == 0) cur_result++;
+        data[weight[node]]++;
     }
     void remove(ll index) {
-        ll edge = eulertour[index];        
-        data[weight[edge]]--;
-        if (data[weight[edge]] == 0) cur_result--;
+        ll node = eulertour[index];        
+        data[weight[node]]--;
+        if (data[weight[node]] == 0) cur_result--;
     }
     void resolve(ll index) {
-        ll edge = eulertour[index];
-        vis[edge] = !vis[edge]; // vis 1 lần thì nó nằm trên đoạn, 2 lần thì nó nằm ngoài đoạn
-        if (!vis[edge]) remove(index);
+        ll node = eulertour[index];
+        vis[node] = !vis[node]; // vis 1 lần thì nó nằm trên đoạn, 2 lần thì nó nằm ngoài đoạn
+        if (!vis[node]) remove(index);
 
-        if (vis[edge]) add(index);
+        if (vis[node]) add(index);
     }
-    void solve(vector<Query> qrs) {
-        queries = fromEdgeToEuler(qrs);
+    void solve(vector<Query>& queries) {
+        transformFromNodeToEuler(queries);
+        for (auto&q : queries) q.ord = hilbertOrder(q.l, q.r);
         sort(queries.begin(), queries.end());
         ll cur_l = 0, cur_r = -1;
         for (auto query: queries) {
@@ -161,7 +172,7 @@ Mo mo(a, queries);
 mo.solve();
 for (auto v: mo.ans) cout << v << ' ';
 */
-int main(){
+signed main(){
     ios::sync_with_stdio(0); cin.tie(0);
     #ifdef DEBUG
         freopen("inp.txt", "r", stdin);
