be6da026ac82c6105e3a9e8bb3fcf1fc62d5cc80
conlacda <phamlong15297@gmail.com>
2023-12-25 23:06:00 +0900

    Update Road Reparation.cpp

--- cses/Graph Algorithms/Road Reparation.cpp
+++ cses/Graph Algorithms/Road Reparation.cpp
@@ -11,80 +11,80 @@ using namespace std;
 #include "debug.cpp"
 #else
 #define dbg(...)
+#define show_exec_time()
+#define destructure(a) #a
 #endif
  
+
 class DSU {
  public:
-  vector<int> parent, _rank;
-  DSU(int N) {
-    this->parent.resize(N);
-    this->_rank.resize(N);
-    for (int i = 0; i < N; i++) {
-      this->make_set(i);
+    vector<int> parent, rank;
+    int N;
+    DSU(int N) {
+        this->N = N;
+        this->parent.resize(N);
+        this->rank.resize(N, 0);
+        for (int i = 0; i < N; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
     }
-  }
- 
-  void make_set(int v) {
-    this->parent[v] = v;
-    this->_rank[v] = 0;
-  }
- 
-  int find_set(int v) {
-    if (v == parent[v]) {
-      return v;
+
+    int find(int v) {
+        assert(0 <= v && v < N);
+        if (v == parent[v]) return v;
+        return parent[v] = find(parent[v]);
     }
-    return parent[v] = find_set(parent[v]);
-  }
- 
-  void merge_set(int a, int b) {
-    a = find_set(a);
-    b = find_set(b);
-    if (a != b) {
-      if (_rank[a] < _rank[b]) {
-        swap(a, b);
-      }
-      parent[b] = a;
-      if (_rank[a] == _rank[b]) {
-        _rank[a]++;
-      }
+
+    void merge(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a != b) {
+            if (rank[a] < rank[b]) swap(a, b);
+            parent[b] = a;
+            if (rank[a] == rank[b]) rank[a]++;
+        }
     }
-  }
+    bool isSame(int a, int b) {
+        return find(a) == find(b);
+    }
 };
-/*
-DSU dsu(N);
-dsu.merge_set(u,v);
-dsu.find_set(u) == dsu.find_set(v); // check if u,v in the same SCC
-*/
+
 struct Edge {
     ll u, v, w;
     friend bool operator<(Edge a, Edge b){
         return a.w < b.w;
     }
+    static bool lexicographic_order(Edge a, Edge b){
+        if (a.u == b.u) return a.v < b.v;
+            return a.u < b.u;    
+    }
+    friend std::ostream& operator<<(std::ostream& os, const Edge& s) { return os << destructure(s);}
 };
 vector<Edge> kruskal(vector<vector<pair<ll,ll>>> g){
     // Nhận vào 1 graph và trả về danh sách của MST
     int n = (int) g.size();
-    // vector<Edge> edges;
-    multiset<Edge> edges;
+    vector<Edge> edges;
     for (int i=0;i<n;i++){
         for (auto pii: g[i]){
-            edges.insert(Edge{i, pii.first, pii.second});
+            edges.push_back(Edge{i, pii.first, pii.second});
         }
     }
-    // sort(edges.begin(), edges.end());
+    sort(edges.begin(), edges.end());
     DSU dsu(n);
     vector<Edge> mst;
     for (auto edge: edges){
-        if (dsu.find_set(edge.u) != dsu.find_set(edge.v)){
+        if (dsu.find(edge.u) != dsu.find(edge.v)){
             mst.push_back(edge);
-            dsu.merge_set(edge.u, edge.v);
+            dsu.merge(edge.u, edge.v);
         }
     }
-    if ((int)mst.size() != n-1){
-        return {};
-    }
+    // sort(mst.begin(), mst.end(), Edge::lexicographic_order); // sắp xếp theo thứ tự đỉnh nhỏ trước
+    if ((int)mst.size() != n-1) return {}; // đồ thị ko connected
     return mst;
 }
+// Doc: http://localhost:3000/docs/competitive-programming/graph/basic/minimum-spanning-tree
+
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
     #ifdef DEBUG
