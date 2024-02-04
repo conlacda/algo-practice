d095e9ce7a507d9bdfacddc15e0c558e63a037bd
conlacda <phamlong15297@gmail.com>
2024-02-05 00:15:23 +0900

    Update G - Christmas Color Grid 2.cpp

--- atcoder/BeginnerContest/334/G - Christmas Color Grid 2.cpp
+++ atcoder/BeginnerContest/334/G - Christmas Color Grid 2.cpp
@@ -449,3 +449,430 @@ signed main(){
 Optimise:
 * chỉ xét cut point để giảm kích thước của segment tree
 */
+
+
+
+//===================================================================
+/*
+// Đoạn code dưới đây xét toàn bộ mọi node để đưa vào dynamic connectivity dẫn tới TLE
+// Cần thêm optimise chỉ xét cutpoint như bên trên thì mới pass
+// Sử dụng template mới. Code trên sử dụng template cũ
+#include<bits/stdc++.h>
+
+typedef long long ll; // ld long double
+const ll mod = 998244353; // 998244353  1000000009  1000000007 // đừng dùng ull
+#define int long long // __int128
+const int INF = std::numeric_limits<int>::max(); // use INT32_MAX for i32
+
+using namespace std;
+
+#ifdef DEBUG
+#include "debug.cpp"
+#else
+#define dbg(...)
+#define show_exec_time()
+#define destructure(a) #a
+#endif
+
+const int maxN = 1000003;
+vector<ll> factorial, factorial_inv; // giai thừa
+template<unsigned int mod = mod>
+struct Mint {
+    friend std::ostream& operator<<(std::ostream& os, const Mint &s) { return os << s.x;}
+    friend std::istream& operator>>(std::istream& os, Mint &s) { return os >> s.x;}
+    unsigned int x;
+ 
+    Mint() : x(0) {}
+    Mint(ll _x) { // auto convert ll to Mint.
+        _x %= mod;
+        if (_x < 0) _x += mod;
+        x = _x;
+    }
+
+    unsigned int val() {
+        return x;
+    }
+
+    Mint& operator += (const Mint &a) {
+        x += a.x;
+        if (x >= mod) x -= mod;
+        return *this;
+    }
+    Mint& operator -= (const Mint &a) {
+        x += mod - a.x;
+        if (x >= mod) x -= mod;
+        return *this;
+    }
+    Mint& operator *= (const Mint &a) {
+        x = (unsigned long long)x * a.x % mod;
+        return *this;
+    }
+    Mint& operator /= (const Mint &a) {
+        return *this *= a.inv();
+    }
+
+    Mint power(ll pw) const {
+        Mint res = 1;
+        Mint cur = *this;
+        while(pw) {
+            if (pw & 1) res *= cur;
+            cur *= cur;
+            pw >>= 1;
+        }
+        return res;
+    }
+    // Chứng minh xem tại CPH - Modular inverse
+    Mint inv() const {
+        assert(x != 0);
+        unsigned int t = x;
+        unsigned int res = 1;
+        while(t != 1) {
+            unsigned int z = mod / t;
+            res = (unsigned long long)res * (mod - z) % mod;
+            t = mod - t * z;
+        }
+        return res;
+    }
+    
+    Mint operator + (const Mint &a) const {
+        return Mint(*this) += a;
+    }
+    Mint operator - (const Mint &a) const {
+        return Mint(*this) -= a;
+    }
+    Mint operator * (const Mint &a) const {
+        return Mint(*this) *= a;
+    }
+    Mint operator / (const Mint &a) const {
+        return Mint(*this) /= a;
+    }
+ 
+    bool operator == (const Mint &a) const {
+        return x == a.x;
+    }
+    bool operator != (const Mint &a) const {
+        return x != a.x;
+    }
+    bool operator < (const Mint &a) const {
+        return x < a.x;
+    }
+    
+    // Hàm này chỉ chạy với n,r < maxN (~ 1e6)
+    Mint nCr(Mint r) {
+        assert(this->x < maxN && r < maxN && "n, r phải nhỏ hơn maxN");
+        if (r < 0 || this->x < r.x) return 0;
+        if (factorial.size() == 0) cal_factor();
+        return Mint(factorial[x]) * Mint(factorial_inv[r.x]) * Mint(factorial_inv[x-r.x]);
+    }
+    Mint nPr(Mint r) {
+        assert(this->x < maxN && r < maxN && "n, r phải nhỏ hơn maxN");
+        if (x < r.x) return 0; // xC(r.x)
+        if (factorial.size() == 0) cal_factor();
+        return Mint(factorial[x]) * Mint(factorial_inv[x-r.x]);
+    }
+private:
+    void cal_factor() { std::function<vector<ll>(int)> mod_inv_range = [&](int sz){ vector<ll> inv(sz, 1); for (int i=2;i<sz;i++){ inv[i] = mod - (mod/i) * inv[mod%i] % mod;} return inv;}; factorial.resize(maxN, 1); factorial_inv.resize(maxN, 1); vector<ll> inv = mod_inv_range(maxN); for (ll i=1;i < (int) factorial.size();i++){ factorial[i] = (factorial[i-1] * i) % mod; factorial_inv[i] = (factorial_inv[i-1] * inv[i]) %mod;}}
+};
+using mint = Mint<mod>;
+
+// Rollback DSU
+struct RollbackDSU {
+    int ccnum; // connected component num
+    vector<int> rank, parent, siz;
+    stack<tuple<int, int, int, int>> op; //a, b, rankA, rankB
+
+    RollbackDSU() {}
+    RollbackDSU(int n) {build(n);};
+    void build(int n) {
+        ccnum = n;
+        rank.resize(n);
+        parent.resize(n);
+        iota(parent.begin(), parent.end(), 0);
+    }
+
+    int find(int v) {
+        return (v == parent[v] ? v : find(parent[v]));
+    }
+
+    void merge(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a == b) return;
+
+        if (rank[a] < rank[b]) swap(a, b);
+        op.push(make_tuple(a, b, rank[a], rank[b]));
+
+        ccnum--;
+        parent[b] = a;
+        rank[a] += rank[a] == rank[b];
+    }
+
+    bool isSame(int a, int b) { return find(a) == find(b); }
+
+    void rollback() {
+        if (op.empty()) return;
+
+        auto [a, b, rankA, rankB] = op.top();
+        op.pop();
+
+        ccnum++;
+        parent[a] = a;
+        parent[b] = b;
+        rank[a] = rankA;
+        rank[b] = rankB;
+    }
+};
+
+
+typedef pair<int,int> Edge;
+
+struct Event {
+    int type; // add, remove
+    Edge edge; // cạnh được thêm/xóa khỏi graph
+    static const int ADD = 1, REMOVE = -1, QUERY = 0;
+    friend std::ostream& operator<<(std::ostream& os, const Event s) { return os << destructure(s);}
+};
+
+struct Node {
+    vector<Edge> edges; // lưu các node tồn tại tại thời điểm này
+};
+
+struct SegmentTree {
+    int n;
+    vector<Node> dat;
+    SegmentTree() {};
+    void build(int _n) {
+        n = (int) pow(2, ceil(log2(_n)));
+        dat.resize(2 * n -1);
+    }
+    // tra ve id cua cac node co lien quan
+    vector<int> query(int l, int r) {
+        assert(l <= r);
+        vector<int> nodeIds;
+        l += n - 1; r += n;
+        while (l < r) {
+            if ((l & 1) == 0) nodeIds.push_back(l);
+            if ((r & 1) == 0) nodeIds.push_back(r-1);
+            l = l / 2; r = (r - 1) / 2;
+        }
+        return nodeIds;
+    }
+    Node& getBaseNode(int ith) { return dat[query(ith, ith).at(0)]; }
+    // tra ve children cua node tren segment tree
+    vector<int> children(int nodeId) { return (nodeId >= 0 && nodeId < n - 1) ? vector<int>{nodeId * 2 + 1, nodeId * 2+ 2} : vector<int>{};}
+    // tra ve parent cua node tren segment tree. nodeId la index trong seg.dat[]
+    int parent(int nodeId) { int p = (nodeId - 1) /2; return (p >= 0 && p < n-1) ? p: -1;}
+    // baseNode là node ở mảng a ban đầu. ko null & ko có children
+    bool isBaseNode(int u) { return !(u >= 0 && u < n - 1);}
+
+    // Edge xuat hien tren do thi tai thoi diem start -> end
+    void addEdge(Edge edge, int start_time, int end_time) {
+        if (edge.first > edge.second) swap(edge.first, edge.second);
+        // Lấy ra mọi node có liên quan rồi đẩy dữ liệu vào đây
+        auto nodeIds = query(start_time, end_time);
+        for (auto nodeId: nodeIds) 
+            dat[nodeId].edges.push_back(edge);
+    }
+};
+
+struct DynamicConnectivity {
+private:
+    int lastEvent = Event::ADD;
+    int event_sz, node_num;
+    // chuyển events sang tập hợp cạnh và thời gian tồn tại
+    vector<tuple<Edge, int, int>> events2Timeline(vector<vector<Event>> events) {
+        vector<tuple<Edge, int, int>> timeline; // tuple<Edge, start, end>
+        map<Edge, int> startTime;
+        for (int time=0;time<event_sz;time++) { // xét toàn bộ sự kiện của từng thời điểm
+            for (auto event: events[time]) {
+                if (event.type == Event::ADD) { // add
+                    startTime[event.edge] = time;
+                } else if (event.type == Event::REMOVE) { // remove
+                    // remove cạnh tức là cạnh này chỉ tồn tại tới thời điểm i hiện tại
+                    if (startTime[event.edge] <= time -1) // nếu sự kiện add + remove cùng 1 cạnh liền nhau thì cơ bản là nó ko tồn tại
+                        timeline.push_back({event.edge, startTime[event.edge], time -1});
+                    startTime.erase(event.edge);
+                }
+            }
+        }
+        // những cạnh được thêm vào nhưng không bao giờ xóa đi -> tồn tại từ lúc thêm vào -> forever
+        int forever = (int) events.size() - 1;
+        for (auto [edge, start_time]: startTime)
+            timeline.push_back({edge, start_time, forever});
+        return timeline;
+    }
+public:
+    SegmentTree seg;
+    RollbackDSU dsu;
+    vector<vector<Event>> events;
+    vector<int> query_indexes;
+    DynamicConnectivity(vector<vector<int>>& adj) {
+        node_num = adj.size();
+        // add initial graph as ADD events
+        events.push_back({});
+        for (int u=0;u<node_num;u++) {
+            for (auto v: adj[u]) {
+                if (u < v) events[0].push_back(Event{Event::ADD, Edge{u, v}});
+            }
+        }
+        lastEvent = Event::ADD;
+        // build dsu
+        dsu.build(node_num);
+    };
+
+    // Add event one by one
+    void addEvent(Event e) {
+        if (e.type == Event::QUERY) {
+            query_indexes.push_back((int)events.size() -1);
+            lastEvent = Event::QUERY;
+            return;
+        }
+        // ADD, REMOVE
+        if ((int) events.size() == 1 || lastEvent == Event::QUERY) events.push_back({}); // events[0] is reserved
+        if (e.edge.first > e.edge.second) swap(e.edge.first, e.edge.second);
+        events[(int) events.size() -1].push_back(e);
+        lastEvent = Event::ADD; // or Event:REMOVE is same
+    }
+    // Build + add all events to segtree's nodes
+    void prepareSegTree() {
+        event_sz = (int) events.size();
+        seg.build(event_sz);
+        auto timeline = events2Timeline(events);
+        for (auto [edge, start, end]: timeline)
+            seg.addEdge(edge, start, end);
+    }
+    /* Chạy DFS() từ đỉnh qua toàn bộ cây.
+     * Tại node cuối (base node) thì lấy ra kết quả
+     * Chỉ những index nào có query sẽ được lấy ra.
+    */
+    vector<int> getAns() {
+        vector<int> resAfterEachEvent; // kết quả sau mỗi cụm events
+        std::function<void(int)> dfsOnSegTree = [&](int u) {
+            // CHANGE HERE // *** // ***
+            int merge_num = 0;
+            // Tới node u thì thêm tất cả các edge đang tồn tại trong node u vào
+            for (auto edge: seg.dat[u].edges) {
+                if (!dsu.isSame(edge.first, edge.second)) {
+                    dsu.merge(edge.first, edge.second);
+                    merge_num++;
+                }
+            }
+            // dfs() tới các child
+            for (auto child: seg.children(u)) dfsOnSegTree(child);
+            // tới baseNode là tầng cuối cùng, nếu event_type là query thì in ra kết quả
+            if (seg.isBaseNode(u)) { // base node
+                resAfterEachEvent.push_back(dsu.ccnum); // lấy ra kết quả ngay sau khi sự kiện xảy ra.
+                // Sau đó có queryIndexes lọc ra các kết quả cần lấy 
+            }
+            // Duyệt xong toàn bộ con thì quay lại xóa các node đã được thêm vào
+            for (int i=0;i<merge_num;i++)
+                dsu.rollback();
+        };
+        dfsOnSegTree(0); // dfs from root = 0
+        // Get needed results
+        vector<int> neededRes;
+        for (auto index: query_indexes) neededRes.push_back(resAfterEachEvent[index]);
+        return neededRes;
+    }
+};
+/*
+DynamicConnectivity dc(vector<vector<int>>& adj);
+AddEvent
+    dc.addEvent(Event{Event::ADD, Edge{u, v}}); // one by one
+    dc.addEvent(Event{Event::QUERY});
+dc.prepareSegTree(); // push edge to segtree's nodes
+dc.getAns(); // run dfs on segtree then collect answer
+*/
+
+// Chuyen doi grid sang graph
+// vector<int> nodeIds; // thuận tiện cho việc duyệt qua các node sau này
+vector<vector<int>> grid2Graph(vector<string>& grid, char node = '#') {
+    int n = grid.size(), m = grid[0].size();
+    vector<vector<int>> adj(n*m);
+    for (int i=0;i<n;i++) {
+        for (int j=0;j<m;j++) {
+            if (grid[i][j] != node) continue;
+            // nodeIds.push_back(i * m + j);
+            if (i+1 < n && grid[i+1][j] == node) {
+                adj[i*m + j].push_back((i+1)*m + j); 
+                adj[(i+1)*m + j].push_back(i*m + j); 
+            }
+            if (j+1 < m && grid[i][j+1] == node) {
+                adj[i*m + j].push_back(i*m + j+1);
+                adj[i*m + j+1].push_back(i*m + j);
+            }
+        }
+    }
+    return adj;
+}
+
+// Đếm số lượng ccnum trên grid
+int connectedComponentNum(vector<string>& grid, char node = '#') {
+    int ccnum = 0;
+    int n = grid.size(), m = grid[0].size();
+    vector<bool> vis(n * m, false);
+    std::function<void(int, int)> dfs = [&](int i, int j){
+        vis[i * m + j] = true;
+        if (i+1 < n && grid[i+1][j] == node && !vis[(i+1) * m + j]) dfs(i+1, j);
+        if (i-1 >= 0 && grid[i-1][j] == node && !vis[(i-1) * m + j]) dfs(i-1, j);
+        if (j+1 < m && grid[i][j+1] == node && !vis[i*m+j+1]) dfs(i, j+1);
+        if (j-1 >= 0 && grid[i][j-1] == node && !vis[i*m+j-1]) dfs(i, j-1);
+    };
+    for (int i=0;i<n;i++) {
+        for (int j=0;j<m;j++) {
+            if (!vis[i*m+j] && grid[i][j] == node) ccnum++, dfs(i, j);
+        }
+    }
+    return ccnum;
+}
+signed main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+#ifdef DEBUG
+    freopen("inp.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    int row, col;
+    cin >> row >> col;
+    vector<string> grid(row);
+    for (auto& v: grid) cin >> v;
+    auto adj = grid2Graph(grid);
+    DynamicConnectivity dc(adj);
+    // Xét từng node -> xóa toàn bộ cạnh của node đó -> query -> thêm toàn bộ cạnh vào node đó
+    // Có 2 loại node:
+    // * Đứng 1 mình -> xóa thì số ccnum giảm đi 1
+    // * Có cạnh -> xử lý bằng xóa cạnh tạo ra event trong dynamic connectivity
+    for (int i=0;i<row*col;i++) {
+        if ((int) adj[i].size() == 0) continue;
+        for (auto v: adj[i])
+            dc.addEvent({Event::REMOVE, Edge{i, v}});
+        
+        dc.addEvent({Event::QUERY});
+        for (auto v: adj[i])
+            dc.addEvent({Event::ADD, Edge{i, v}});
+    }
+    dc.prepareSegTree();
+    int unexpectedCcnum = 0; // số dấu . được đếm thành 1 cc
+    for (auto v: grid) unexpectedCcnum += std::count(v.begin(), v.end(), '.');
+    mint sum_ccnum = 0;
+    for (auto v: dc.getAns()) {
+        sum_ccnum += v - unexpectedCcnum - 1;
+    }
+    dbg(sum_ccnum);
+    // ***
+    // đoạn này đang sai ko biết sao dsu tính ccnum lại sai
+    dbg(dc.getAns(),unexpectedCcnum);
+    // xử lý node đứng 1 mk -> mất đi thì số lượng ccnum - 1
+    int aloneNodeNum = 0;
+    for (int i=0;i<row;i++) {
+        for (int j=0;j<col;j++) {
+            if (grid[i][j] == '#' && adj[i*col+j].size() == 0) aloneNodeNum++;
+        }
+    }
+    // mỗi cái alone tạo ra dc.dsu.ccnum - unexpectedCcnum - 1 cc
+    int ccnum = connectedComponentNum(grid, '#');
+    sum_ccnum += (ccnum - 1) * aloneNodeNum;
+    cout << sum_ccnum / (row*col - unexpectedCcnum);
+
+    show_exec_time();
+}
+*/
