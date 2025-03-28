6de1e345417672590f80919cb9386927dbf9fa8b
conlacda <phamlong15297@gmail.com>
2025-03-28 12:49:39 +0900

    Update Searching for Strings.cpp

--- DMOJ/Searching for Strings.cpp
+++ DMOJ/Searching for Strings.cpp
@@ -1,4 +1,5 @@
 // https://dmoj.ca/problem/ccc20s3
+// Submit via vjudge
 #include<bits/stdc++.h>
 
 typedef long long ll; // double long double
@@ -14,6 +15,35 @@ using namespace std;
 #define dbg(...)
 #endif
 
+mt19937 _rand(std::chrono::steady_clock::now().time_since_epoch().count());
+map<int, int> randomMap;
+void randomMapping() {} // Base case: when there are no more arguments to process
+template <typename T, typename... Args>
+void randomMapping(T& first, Args&... rest) {
+    for (auto&v: first) {
+        if (!randomMap.count(v)) {
+            auto randomVal = _rand();
+            randomMap[v] = randomVal;
+            v = randomVal;
+        } else {
+            v = randomMap[v];
+        }
+    }
+
+    randomMapping(rest...);
+}
+
+vector<int> factor;
+const int maxN = 200005;
+void precompute() {
+    factor.resize(maxN);
+    int cur = 1;
+    for (int i=0;i<maxN;i++) {
+        factor[i] = cur;
+        cur = (cur * 263) % mod;
+    }
+}
+
 template<typename Node>
 class SegmentTree {
 private:
@@ -21,7 +51,7 @@ private:
     vector<Node> dat;
 public:
     SegmentTree() {}
-    SegmentTree(const vector<Node>& v){ build(v);}
+    SegmentTree(const vector<Node>& v) { build(v);}
     void build(const vector<Node>& v) {
         n = 1; while (n < (int) v.size()) n *= 2;
         dat.resize(2 * n - 1, Node{.is_null = true});
@@ -36,6 +66,7 @@ public:
             dat[index] = dat[index * 2 + 1] + dat[index * 2 + 2];
         }
     }
+    template<class... P> void set(int index, P... params) { set(index, Node{params...});}
     Node query(int l, int r){
         assert(l <= r);
         Node left = Node{.is_null = true}, right = Node{.is_null = true};
@@ -47,80 +78,67 @@ public:
         }
         return left + right;
     }
-};
-
-struct Node {
-    int sum = 0;
-    int _xor = 0;
-    bool is_null = false;
-    friend Node operator+(Node a, Node b) {
-        if (a.is_null) return b;
-        if (b.is_null) return a;
-        return Node{
-            a.sum + b.sum,
-            a._xor ^ b._xor
-        };
-    }
-};
-
-template<class Iterable = string> // chỉ chạy với 64bit.
-class Hash {
-private:
-    const ll factor = 263; // số nguyên tố lớn hơn 255 - là full bảng Acsii
-    const char min_char = 0x00; // kí tự 0
-    vector<ll> inv, pc; // pre-compute
-    ll n;
-    // a*x ≡ 1 mod m -> find x - xem thêm tại math-compilation snippet
-    ll mod_inv(ll a) { ll x, y;auto extended_gcd = [&] (ll a, ll b) -> ll { x = 1; y =0; ll x1 = 0, y1 = 1, a1 = a, b1 = b; while (b1) {ll q = a1 / b1;tie(x, x1) = make_tuple(x1, x - q * x1);tie(y, y1) = make_tuple(y1, y - q * y1);tie(a1, b1) = make_tuple(b1, a1 - q * b1);}return a1;};ll g = extended_gcd(a, mod);if (g != 1) return -1;else x = (x%mod +mod) %mod;return x;}
-    int toInt(char x) { // char(127) = 127 nhưng char(128) = -127, ... char(255) = -1 
-        int valOfChar = x - min_char + 1;
-        int res = (valOfChar <= 0) ? valOfChar + 256 : valOfChar;
-        assert(res > 0 && "min_char phải nhỏ hơn s[i]");
+    // Retrieve current state of vector
+    vector<Node> original() {
+        vector<Node> res;
+        for (int i=0;i<n;i++)
+            res.push_back(query(i, i));
         return res;
     }
-    int toInt(int x) {
-        return x - min_char + 1;
+
+    Node operator[](int index) {
+        assert(0 <= index && index < n && "Out of range");
+        return query(index, index);
     }
-public:
-    Iterable s;
-    vector<ll> prefix_hash, suffix_hash;
-    void build(ll length = 250000){ ll p = 1; for (ll i=0;i<length;i++){ pc.push_back(p); p = (p* factor) % mod;} for (auto v: pc) inv.push_back(mod_inv(v));}
-    // Precompute O(N) dạng prefix sum để sau tính hash từ l->r với O(1).     
-    void load(Iterable s) {
-        assert((int) pc.size() >= (int) s.size() && "quên chưa hash.build()??");
-        this->s = s; this->n = (ll) s.size();
-        prefix_hash = suffix_hash = {0};
-        ll pre = 0, suf = 0;
-        for (int i=0;i< (int) s.size();i++) {
-            pre = (pre + toInt(s[i]) * pc[i]) % mod;
-            suf = (suf + toInt(s[(int) s.size() - i - 1]) * pc[i]) % mod;
-            prefix_hash.push_back(pre);
-            suffix_hash.push_back(suf);
+
+    // return min index r that seg.query(start, r) satisfies the condition
+    // return -1 if not found
+    int find_right(int val, int start = 0) {
+        int l = start, r = n - 1;
+        while (l != r) {
+            int mid = (l + r) / 2;
+            if (query(start, mid).val >= val) r = mid;
+            else l = mid + 1;
         }
+        if (query(start, l).val >= val)
+            return l;
+        return -1;
     }
-    // Lấy ra hash của s.substr(start, length)
-    ll substr(ll start, ll length) {
-        assert(length <= n); // assert(start+length <= n); nếu chỉ muốn range thông thường ko phải dạng rolling
-        ll end = (start + length > n) ? (start + length - n -1) : (start+length-1);
-        // hash dạng rolling substr- tức là nếu start+length> s.size() thì sẽ vòng lại lấy từ đầu đi tiếp
-        std::function<int(ll, ll, vector<ll>&)> hashOfSubstr = [&](ll start, ll length, vector<ll>& ps_hash){
-            ll ans = 0;
-            if (start + length <= n) {
-                ans = (ps_hash[start + length] - ps_hash[start] + mod) % mod;
-                return (ans * inv[start]) % mod;
-            }
-            ll start2ssize = (ps_hash[n] - ps_hash[start] + mod) % mod;
-            start2ssize = (start2ssize * inv[start]) % mod;
-            ll zero2end = ps_hash[length + start - n];
-            ans = (start2ssize + zero2end * pc[n - start]) % mod;
-            if (ans < 0) ans += mod;
-            return ans;
-        };
-        return (ll) hashOfSubstr(start, length, prefix_hash) << 32 | hashOfSubstr(n - 1 - end, length, suffix_hash);
+
+    // tìm ra index lớn nhất để seg.query(index, end) >= val
+    // không tìm thấy sẽ trả về -1
+    int find_left(int val, int end = -1) {
+        if (end == -1) end = n - 1;
+        int l = 0, r = end;
+        while (l != r) {
+            int mid = (l + r) / 2;
+            if (mid * 2 < l + r) mid++;
+            if (query(mid, end).val >= val) l = mid;
+            else r = mid - 1;
+        }
+        if (query(l, end).val >= val)
+            return l;
+        return -1;
     }
 };
 
-mt19937 _rand(std::chrono::steady_clock::now().time_since_epoch().count());
+struct SumXorNode {
+    int sum = 0, _xor = 0;
+    int size = 1, hash = 0, rhash = 0;
+    bool is_null = false;
+    friend SumXorNode operator+(SumXorNode a, SumXorNode b) {
+        if (a.is_null) return b;
+        if (b.is_null) return a;
+        return SumXorNode {
+            a.sum + b.sum,
+            a._xor ^ b._xor,
+            a.size + b.size,
+            (a.hash + factor[a.size] * b.hash) % mod,
+            (b.rhash + factor[b.size] * a.rhash) % mod
+        };
+    }
+};
+using Node = SumXorNode;
 
 signed main() {
     ios::sync_with_stdio(0); cin.tie(0);
@@ -129,38 +147,35 @@ signed main() {
     freopen("inp.txt", "r", stdin);
     freopen("out.txt", "w", stdout);
 #endif
-    string a, b;
-    cin >> a >> b;
-    Hash<string> hsh;
-    hsh.build((int) b.size() + 5);
-    hsh.load(b);
-    map<char, int> m;
-    for (auto&v: a) {
-        if (!m.contains(v))
-            m[v] = _rand();
+    precompute();
+    string s, b;
+    cin >> s >> b;
+    vector<int> vs(s.begin(), s.end());
+    vector<int> vb(b.begin(), b.end());
+    randomMapping(vs, vb);
+    /*
+    Query toàn bộ b để xem hash(start, n) có bằng với hash(s) hay không
+    Nếu bằng thì cần lưu lại nó bằng 1 giá trị hash thông thường
+    */
+    // Tính hash cho small string
+    Node small = Node{.is_null = true};
+    for (auto&v: vs) {
+        small = small + Node{.sum = v, ._xor = v, .hash = v, .rhash = v};
     }
-    for (auto&v: b) {
-        if (!m.contains(v))
-            m[v] = _rand();
+    SegmentTree<Node> seg;
+    vector<Node> si;
+    for (int i=0;i<(int) vb.size();i++) {
+        si.push_back(Node{.sum = vb[i], ._xor = vb[i], .hash = vb[i], .rhash = vb[i]});
     }
-    vector<Node> aa(a.size()), bb(b.size());
-    for (int i=0;i< (int) a.size();i++) aa[i] = Node{m[a[i]], m[a[i]]};
-    for (int i=0;i< (int) b.size();i++) bb[i] = Node{m[b[i]], m[b[i]]};
-    SegmentTree<Node> seg1(aa), seg2(bb);
-    auto node = seg1.query(0, (int) aa.size() - 1);
-    int ans = 0;
-    set<int> matched;
-    for (int i=0;i<=(int) bb.size() - (int) aa.size();i++) {
-        auto node2 = seg2.query(i, i + (int) a.size() - 1);
-        if (node.sum == node2.sum and node._xor == node2._xor) {
-            auto hash_value = hsh.substr(i, a.size());
-            if (matched.count(hash_value) == 0) {
-                ans++;
-                matched.insert(hsh.substr(i, a.size())); 
-            }
+    seg.build(si);
+    set<pair<int,int>> _set;
+    for (int i=0;i<(int) b.size() - (int) s.size() + 1;i++) {
+        auto node = seg.query(i, i + (int) s.size() - 1);
+        if (node.sum == small.sum and node._xor == small._xor) {
+            _set.insert({node.hash, node.rhash});
         }
     }
-    cout << ans;
+    cout << _set.size();
     /*
     Nếu lặp qua toàn bộ permutation(a) rồi check trong string b thì quá chậm
     Sử dụng hash thì mỗi string a lặp qua b mất O(N)
@@ -173,6 +188,6 @@ signed main() {
     do đó mỗi khi match được 1 substring trên b thì thêm hash của nó vào trong set
     khi hash của subtr ko có trong set thì mới đếm lên 1
     */
-    
+
     cerr << "Time : " << (double)clock() / (double)CLOCKS_PER_SEC << "s✅\n";
 }
